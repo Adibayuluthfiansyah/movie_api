ce28ba4b5f8cc39350fcee19fb13364a54fe555c
Adibayu Luthfiansyah Setyawan <106433930+Adibayuluthfiansyah@users.noreply.github.com>
2025-07-12 22:09:10 +0700

    add loading animation

---
+++ src/loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+        <div className='flex justify-center items-center min-h-screen'>
+
+        <div className='spinner justify-center items-center'></div>
+
+    </div>
+  )
+}
+
+export default loading
