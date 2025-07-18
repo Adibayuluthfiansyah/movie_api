d5c6685d14b9f782abf65992860eb2cff0348b82
Adibayu Luthfiansyah Setyawan <106433930+Adibayuluthfiansyah@users.noreply.github.com>
2025-07-19 00:01:49 +0700

    add tvshows and upcoming

--- src/app/component/movieList/Header.tsx
+++ src/app/component/movieList/Header.tsx
@@ -11,7 +11,7 @@ interface movieTitleProps {
 const Header = ({title, linkHref, linkTitle}: movieTitleProps) => {
   return (
     <div className='flex justify-between items-center p-4'>
-    <h1 className="ml-29 mt-2 text-2xl font-bold">{title}</h1>
+    <h1 className="ml-29 mt-2 text-2xl font-bold text-red-600">{title}</h1>
     <Link href={linkHref} className='md:text-xl mr-15 text-sm underline hover:text-red-600 transition-all'>
     {linkTitle}
     </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,33 @@
 import Header from "./component/movieList/Header";
 import MovieList from "./component/movieList/page";
 
-
 export default async function Home() {
-  const API_KEY = process.env.NEXT_PUBLIC_API_KEY
+    const API_KEY = process.env.NEXT_PUBLIC_API_KEY
     const response = await fetch (`${process.env.NEXT_PUBLIC_API_POPULAR_URL}?api_key=${API_KEY}`)
     const topMovie = await response.json()
-
+    
   return (
-    // popular movie
+    
     <div>
+       {/* POPULAR MOVIE */}
       <section className="mt-12 p-2">
       <Header title={"Most Popular"} linkTitle={"See All"} linkHref={"/popular"}/>
       <MovieList api= {topMovie}/>
       </section>
-    </div>
 
+      {/* UPCOMING */}
+      <section className="mt-12 p-2">
+      <Header title={"UPCOMING"} linkTitle={"See All"} linkHref={"/popular"}/>
+      <MovieList api= {topMovie}/>
+      </section>
+
+      {/* TV SHOWS */}
+      <section className="mt-12 p-2">
+      <Header title={"TV SHOWS"} linkTitle={"See All"} linkHref={"/popular"}/>
+      <MovieList api= {topMovie}/>
+      </section>
+
+    </div>
     
   );
 }
