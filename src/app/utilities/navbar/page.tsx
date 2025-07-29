906cb07746da7a6220f0d5cd1ba53bda245ab950
Adibayu Luthfiansyah Setyawan <106433930+Adibayuluthfiansyah@users.noreply.github.com>
2025-07-29 22:30:29 +0700

    fix inconsistent page popular and most popular

--- src/app/utilities/navbar/page.tsx
+++ src/app/utilities/navbar/page.tsx
@@ -28,7 +28,7 @@ const Navbar = () => {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Link href="/" className="text-red-600 text-2xl font-bold">
-              70NGFLIX
+              70NGMOV
             </Link>
           </div>
 
