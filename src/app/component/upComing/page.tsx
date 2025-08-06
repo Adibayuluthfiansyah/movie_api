00af4ebbabdf7d94967843e238aacaa77a869237
Adibayu Luthfiansyah Setyawan <106433930+Adibayuluthfiansyah@users.noreply.github.com>
2025-08-07 00:10:09 +0700

    fix  upcoming list

--- src/app/component/upComing/page.tsx
+++ src/app/component/upComing/page.tsx
@@ -15,16 +15,24 @@ const UpComingList = ({api} : UpComingListProps) => {
       return (
       <div key={movie.id} className='group'>
       <div className='items-center justify-center text-center'>
-        <div>
+        <div className="relative overflow-hidden rounded-lg">
         <Link href={`/${movie.id}`}>
         <img src={`${process.env.NEXT_PUBLIC_API_IMG_URL}${movie.poster_path}`} 
         width={350} 
         height={350} 
-        className='cursor-pointer'
+        className='w-full h-80 object-cover group-hover:scale-105 transition-transform duration-300 cursor-pointer'
         />
+        
+        {/* Hover overlay */}
+        <div className="absolute inset-0 bg-opacity-0 group-hover:bg-opacity-50 transition-all duration-300 flex items-center justify-center">
+        <span className="text-white font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            ▶ Watch
+        </span>
+        </div>
         </Link>
         </div>
         <h1 className='mt-4'>{movie.title} </h1>
+        <p className="text-gray-400 text-xs"></p>
       </div>
     </div>
       )
@@ -34,32 +42,3 @@ const UpComingList = ({api} : UpComingListProps) => {
 }
 
 export default UpComingList
-
-
- 
-    // {api.results?.map ((movie:any) => {
-    //   return (
-    //   <div key={movie.id} className="group">
-    //   <div className='items-center justify-center text-center'>
-    //     <Link href={`/${movie.id}`}>
-    //     <div className="relative overflow-hidden rounded-lg">
-    //     <img src={`${process.env.NEXT_PUBLIC_API_IMG_URL}${movie.poster_path}`} 
-    //     width={350} 
-    //     height={350} 
-    //     className='w-full h-80 object-cover group-hover:scale-105 transition-transform duration-300'
-    //     />
-    //     {/* Hover overlay */}
-    //     <div className="absolute inset-0 bg-opacity-0 group-hover:bg-opacity-50 transition-all duration-300 flex items-center justify-center">
-    //     <span className="text-white font-semibold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-    //       ▶ Watch
-    //     </span>
-    //     </div>
-    //     </div>
-    //     </Link>
-    //     <h1 className='mt-4'>{movie.title} </h1>
-    //     <p className="text-gray-400 text-xs"></p>
-    //   </div>
-    // </div>
-    //   )
-    // })}
-    // </div>
