7268307616a79614808d177bf253395c4e49f8db
Adibayu Luthfiansyah Setyawan <106433930+Adibayuluthfiansyah@users.noreply.github.com>
2025-11-02 03:08:19 +0700

    fix props type at search

--- src/app/search/[keyword]/page.tsx
+++ src/app/search/[keyword]/page.tsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import MovieList from '../../component/movieList/MovieList'
+
 interface SearchPageProps {
-    params : {
-        keyword : string
-    }
+    params: Promise<{
+        keyword: string
+    }>
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }
 
-const Page =  async ({params}:SearchPageProps) => {
+const Page = async ({params}: SearchPageProps) => {
     const {keyword} = await params
     const decodedKeyword = decodeURIComponent(keyword) 
     const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SEARCH_URL}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&query=${decodedKeyword}`
+        `${process.env.NEXT_PUBLIC_API_SEARCH_URL}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&query=${decodedKeyword}`
     )
     const searchMovie = await response.json()
-  return (
+    
+    return (
         <div className='justify-center items-center p-4 mt-12'>
-        <h1 className='p-4 justify-center text-center text-2xl font-bold'>Searching for {decodedKeyword}</h1>
-        <MovieList api={searchMovie}/>
-    </div>
-  )
+            <h1 className='p-4 justify-center text-center text-2xl font-bold'>Searching for {decodedKeyword}</h1>
+            <MovieList api={searchMovie}/>
+        </div>
+    )
 }
 
 export default Page
