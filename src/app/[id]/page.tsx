9f13068740df41130993ed7c5d38a989daea192c
Adibayu Luthfiansyah Setyawan <106433930+Adibayuluthfiansyah@users.noreply.github.com>
2025-11-02 02:08:31 +0700

    fix type at moviedetail page

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -2,13 +2,8 @@ import { getMovieDetailResponse } from "../lib/api";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
-// interface MovieDetailProps {
-//   params: { 
-//     id: string;
-//   };
-// }
 
-export default async function MovieDetail({ params }: { params: { id: string } }) {
+export default async function MovieDetail({params}: {params:{id:string}}) {
   const { id } =  await params;
   
   try {
